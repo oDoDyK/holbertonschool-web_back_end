4e12a59af5e542c5206296d58e57d49957459490
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-04-07 12:27:18 +0300

    Refactor HTTP server to handle student data

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,22 +1,67 @@
-const http=require('http')
-const fs=require('fs')
-const server=http.createServer((req,res)=>{
-    const {url,method,headers}=req
-    if(url==='/' && method==='GET'){
-        fs.readFile('index.html',(err,data)=>{
-            if(err){
-                res.wirteHead(500,{'Content-Type':'text/plain'})
-                res.end('Server Err')
-            }
-            res.statusCode=200
-            res.setHeader('Content-Type','text/html')
-            res.end(data)
-        })
-    }else if(url==='/users' &&method ==='GET'){
-        res.writeHead(200,{'Content-Type':'application/json'})
-        res.end(JSON.stringify({name:'xia'}))
-    }else if(method==='GET'&&headers.accept.indexOf('image/*')!==-1){
-        fs.createReadStream('.'+url).pipe(res)
-    }
-})
-server.listen(3000)
+const http = require('http');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(Error('Cannot load the database'));
+      // split data and taking only list without header
+      const lines = data.split('\n').slice(1, -1);
+      // give the header of data
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      // find firstname and field index
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
+      // declarate two dictionaries for count each fields and store list of students
+      const fields = {};
+      const students = {};
+      // it will contain all data
+      const all = {};
+
+      lines.forEach((line) => {
+        const list = line.split(',');
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]]
+          ? `, ${list[idxFn]}`
+          : list[idxFn];
+      });
+
+      all.numberStudents = `Number of students: ${lines.length}\n`;
+      all.listStudents = [];
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const element = fields[key];
+          all.listStudents.push(`Number of students in ${key}: ${element}. List: ${students[key]}`);
+        }
+      }
+      return resolve(all);
+    });
+  });
+}
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') res.end('Hello Holberton School!');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.write(data.numberStudents);
+        res.write(data.listStudents.join('\n'));
+        res.end();
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
+  }
+});
+
+app.listen(port, hostname);
+
+module.exports = app;
