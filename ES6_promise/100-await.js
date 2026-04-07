b67f449b03346ce2c7167536b9972faf7c9e0b63
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-04-07 11:57:46 +0300

    Refactor asyncUploadUser to use ES6 syntax

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -1,28 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
 
-// 100-await.js
-const { uploadPhoto, createUser } = require('./utils');
-async function asyncUploadUser() {
-  // Create an object with photo and user properties initialized to null
-  const result = {
-    photo: null,
-    user: null,
-  };
-  // Use a try/catch block to handle errors
+export default async function asyncUploadUser() {
   try {
-    // Call the uploadPhoto and createUser functions and await their results
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    // Assign the results to the object properties
-    result.photo = photo;
-    result.user = user;
-  } catch (error) {
-    // Log the error to the console
-    console.error(error);
+    const resFromUploadPhoto = await uploadPhoto();
+    const resFromCreateUser = await createUser();
+    return {
+      photo: resFromUploadPhoto,
+      user: resFromCreateUser,
+    };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null,
+    };
   }
-  // Return the result object
-  return result;
 }
-
-asyncUploadUser().then((result) => {
-    console.log(result);
-    });
