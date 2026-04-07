31778fcd71212e8d2d33f345e99845a301b27c3d
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-04-07 12:36:59 +0300

    Refactor countStudents function and server response

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,65 +3,61 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-      if (err) return reject(Error('Cannot load the database'));
-      // split data and taking only list without header
-      const lines = data.split('\n').slice(1, -1);
-      // give the header of data
-      const header = data.split('\n').slice(0, 1)[0].split(',');
-      // find firstname and field index
-      const idxFn = header.findIndex((ele) => ele === 'firstname');
-      const idxFd = header.findIndex((ele) => ele === 'field');
-      // declarate two dictionaries for count each fields and store list of students
-      const fields = {};
-      const students = {};
-      // it will contain all data
-      const all = {};
+    if (!path) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
-      lines.forEach((line) => {
-        const list = line.split(',');
-        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
-        fields[list[idxFd]] += 1;
-        if (!students[list[idxFd]]) students[list[idxFd]] = '';
-        students[list[idxFd]] += students[list[idxFd]]
-          ? `, ${list[idxFn]}`
-          : list[idxFn];
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      const fields = {};
+      students.forEach((student) => {
+        const parts = student.split(',');
+        const firstName = parts[0];
+        const field = parts[3];
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstName);
       });
 
-      all.numberStudents = `Number of students: ${lines.length}\n`;
-      all.listStudents = [];
-      for (const key in fields) {
-        if (Object.hasOwnProperty.call(fields, key)) {
-          const element = fields[key];
-          all.listStudents.push(`Number of students in ${key}: ${element}. List: ${students[key]}`);
-        }
+      let output = `Number of students: ${students.length}`;
+      for (const [field, names] of Object.entries(fields)) {
+        output += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
       }
-      return resolve(all);
+
+      resolve(output);
     });
   });
 }
 
-const hostname = '127.0.0.1';
-const port = 1245;
-
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.end('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.write(data.numberStudents);
-        res.write(data.listStudents.join('\n'));
-        res.end();
+
+  if (req.url === '/') {
+    res.writeHead(200);
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const dbPath = process.argv[2];
+    countStudents(dbPath)
+      .then((result) => {
+        res.writeHead(200);
+        res.end(`This is the list of our students\n${result}`);
       })
       .catch((err) => {
-        res.end(err.message);
+        res.writeHead(200);
+        res.end(`This is the list of our students\n${err.message}`);
       });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
   }
 });
 
-app.listen(port, hostname);
+app.listen(1245);
 
 module.exports = app;
